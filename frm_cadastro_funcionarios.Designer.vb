09eb807fdd34dcf58d4dd4f737780bcb3ebaa1f8
AndreMendonca <andremendonca0504@gmail.com>
2023-11-02 12:26:13 -0300

    Adicionar arquivos de projeto.

---
+++ frm_cadastro_funcionarios.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_cadastro_funcionarios
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.txt_senha = New System.Windows.Forms.TextBox()
+        Me.txt_usuario = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btn_cadastro = New System.Windows.Forms.Button()
+        Me.cmb_acesso = New System.Windows.Forms.ComboBox()
+        Me.btn_excluir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'txt_senha
+        '
+        Me.txt_senha.Location = New System.Drawing.Point(203, 146)
+        Me.txt_senha.Name = "txt_senha"
+        Me.txt_senha.Size = New System.Drawing.Size(208, 22)
+        Me.txt_senha.TabIndex = 1
+        '
+        'txt_usuario
+        '
+        Me.txt_usuario.Location = New System.Drawing.Point(203, 71)
+        Me.txt_usuario.Name = "txt_usuario"
+        Me.txt_usuario.Size = New System.Drawing.Size(202, 22)
+        Me.txt_usuario.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(127, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Usuario:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(127, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Senha:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(127, 224)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Acesso:"
+        '
+        'btn_cadastro
+        '
+        Me.btn_cadastro.Location = New System.Drawing.Point(203, 300)
+        Me.btn_cadastro.Name = "btn_cadastro"
+        Me.btn_cadastro.Size = New System.Drawing.Size(139, 34)
+        Me.btn_cadastro.TabIndex = 7
+        Me.btn_cadastro.Text = "SALVAR"
+        Me.btn_cadastro.UseVisualStyleBackColor = True
+        '
+        'cmb_acesso
+        '
+        Me.cmb_acesso.FormattingEnabled = True
+        Me.cmb_acesso.Items.AddRange(New Object() {"admin", "usuario"})
+        Me.cmb_acesso.Location = New System.Drawing.Point(203, 224)
+        Me.cmb_acesso.Name = "cmb_acesso"
+        Me.cmb_acesso.Size = New System.Drawing.Size(202, 24)
+        Me.cmb_acesso.TabIndex = 8
+        '
+        'btn_excluir
+        '
+        Me.btn_excluir.Location = New System.Drawing.Point(364, 300)
+        Me.btn_excluir.Name = "btn_excluir"
+        Me.btn_excluir.Size = New System.Drawing.Size(104, 34)
+        Me.btn_excluir.TabIndex = 9
+        Me.btn_excluir.Text = "EXCLUIR"
+        Me.btn_excluir.UseVisualStyleBackColor = True
+        Me.btn_excluir.Visible = False
+        '
+        'frm_cadastro_funcionarios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1072, 683)
+        Me.Controls.Add(Me.btn_excluir)
+        Me.Controls.Add(Me.cmb_acesso)
+        Me.Controls.Add(Me.btn_cadastro)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_usuario)
+        Me.Controls.Add(Me.txt_senha)
+        Me.Name = "frm_cadastro_funcionarios"
+        Me.Text = "Gerenciamento de Usuários"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents txt_senha As TextBox
+    Friend WithEvents txt_usuario As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btn_cadastro As Button
+    Friend WithEvents cmb_acesso As ComboBox
+    Friend WithEvents btn_excluir As Button
+End Class
---
+++ frm_cadastro_funcionarios.vb
@@ -0,0 +1,33 @@
+﻿Public Class frm_cadastro_funcionarios
+    Private Sub btn_cadastro_Click(sender As Object, e As EventArgs) Handles btn_cadastro.Click
+        Try
+            SQL = "select * from tb_usuarios where usuario='" & txt_usuario.Text & "'"
+            rs = db.Execute(SQL)
+            If rs.EOF = True Then
+                SQL = "insert into tb_usuarios(usuario, senha, admin) values ('" & txt_usuario.Text & "' , '" & txt_senha.Text & "' , '" & cmb_acesso.Text & "')"
+                rs = db.Execute(SQL)
+                MsgBox("Usuario cadastrado com sucesso!")
+                limpar_campos_cadastro()
+            End If
+        Catch ex As Exception
+            MsgBox("Erro ao gravar dados!")
+        End Try
+    End Sub
+
+    Private Sub cadastro_funcionarios_Load(sender As Object, e As EventArgs) Handles Me.Load
+        conectar_banco()
+    End Sub
+
+    Private Sub txt_usuario_LostFocus(sender As Object, e As EventArgs) Handles txt_usuario.LostFocus
+        Try
+            SQL = "select * from tb_usuarios where usuario='" & txt_usuario.Text & "'"
+            rs = db.Execute(SQL)
+            If rs.EOF = False Then
+                txt_usuario.Text = rs.Fields(1).ToString
+                txt_senha.Text = rs.Fields(2).ToString
+            End If
+        Catch ex As Exception
+            MsgBox("tmj")
+        End Try
+    End Sub
+End Class
